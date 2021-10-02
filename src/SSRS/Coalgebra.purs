391663e10180d01dd65d7b54234c90ecb211a64b
PureFunctor <purefunctor@gmail.com>
2021-10-02 12:41:21 +0800

    Implement apomorphism and futumorphism

--- src/SSRS/Coalgebra.purs
+++ src/SSRS/Coalgebra.purs
@@ -1,3 +1,5 @@
 module SSRS.Coalgebra where
 
 type Coalgebra f a = a → f a
+
+type GCoalgebra n f a = a → f (n a)
--- src/SSRS/Unfold.purs
+++ src/SSRS/Unfold.purs
@@ -1,11 +1,14 @@
 module SSRS.Unfold where
 
-import Data.Either (Either(..))
+import Prelude
+
+import Control.Monad.Free (Free, resume)
+import Data.Either (Either(..), either)
 import Data.Functor.Mu (Mu(..))
 import Data.List (List(..), (:))
 import Data.Tuple (Tuple(..))
 import Dissect.Class (class Dissect, right)
-import SSRS.Coalgebra (Coalgebra)
+import SSRS.Coalgebra (Coalgebra, GCoalgebra)
 
 ana ∷ ∀ p q v. Dissect p q ⇒ Coalgebra p v → v → Mu p
 ana coalgebra seed = go (right (Left (coalgebra seed))) Nil
@@ -23,3 +26,15 @@ ana coalgebra seed = go (right (Left (coalgebra seed))) Nil
 
 postpro ∷ ∀ p q v. Dissect p q ⇒ (p ~> p) → Coalgebra p v → v → Mu p
 postpro post coalgebra = ana (post <<< coalgebra)
+
+apo ∷ ∀ p q v. Dissect p q ⇒ GCoalgebra (Either (Mu p)) p v → v → Mu p
+apo gCoalgebra = ana coalgebra <<< Right
+  where
+  coalgebra ∷ Either (Mu p) v → p (Either (Mu p) v)
+  coalgebra = either (\(In n) → map Left n) gCoalgebra
+
+futu :: forall p q v. Dissect p q => GCoalgebra (Free p) p v -> v -> Mu p
+futu gCoalgebra = ana coalgebra <<< pure
+  where
+  coalgebra :: Free p v -> p (Free p v)
+  coalgebra = either identity gCoalgebra <<< resume
